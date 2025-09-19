019ed2327def23ae19820af17dcda393b26cc323
Mikhail Ilyn <wosco@wosco.pro>
2025-09-20 06:24:18 +0800

    added maplibre and deckgl

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,11 +1,21 @@
-export default defineNuxtConfig({
+export default {
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/seo'
   ],
   site: {
     url: 'https://tvojservis.ru'
+  },
+  css: [
+    'maplibre-gl/dist/maplibre-gl.css'
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        'mapbox-gl': 'maplibre-gl'
+      }
+    }
   }
-})
+}
 
 
